28e21ca9c5f48834f860a0465765d5a92bc72532
meersen <>
2025-06-26 18:18:19 +0200

    UART Test

--- src/main.c
+++ src/main.c
@@ -5,7 +5,16 @@ typedef uint32_t size_t;
 
 extern char BSS_START[], BSS_END[], STACK_TOP[];
 
+volatile char *UART = (char *)0x10000000;
+
 void kernel_main(void) {
+  *UART = 'H';
+  *UART = 'e';
+  *UART = 'l';
+  *UART = 'l';
+  *UART = 'o';
+  *UART = '!';
+  *UART = 10;
   for (;;) {
     __asm__ __volatile__("wfi");
   }
