61363e21283de99773b917af268e696db8b9863b
meersen <>
2025-07-07 19:10:29 +0200

    Enabling interrupts

--- src/boot.s
+++ src/boot.s
@@ -11,4 +11,12 @@ boot:
   la t0, handle_interrupt
   csrw stvec, t0
 
+  # enable external + software + timer interrupts
+  li t0, (1 << 9) + (1 << 5) + (1 << 1)
+  csrw sie, t0
+  csrs sstatus, (1 << 1)
+
+  # for testing - causes software interrupt
+  # csrs sip, 0b10
+
   j kernel_main
