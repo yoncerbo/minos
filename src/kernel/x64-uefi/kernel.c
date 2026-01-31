b845673f8295290115a0d3a12fa8c549a374fd96
meersen <>
2026-01-31 18:17:35 +0100

    Fixed loading and running user elf programs

--- src/kernel/x64-uefi/kernel.c
+++ src/kernel/x64-uefi/kernel.c
@@ -46,11 +46,14 @@ void _start(BootData *data) {
   };
 
   // NOTES:
-  // Virtual memory regions from the bootloader:
+  // Virtual memory maps from the bootloader:
+  // physical memmory ofseted by HIGHER_HALF
   // kernel code and data at -2GiB
   // kernel stack right below the kernel base
   // boot data above HIGHER_HALF
   // bootloader image code and data - to reuse
+
+  // TODO: Map all physical memory
 
   MemoryManager mm = {
     .page_alloc = &page_alloc,
@@ -67,7 +70,9 @@ void _start(BootData *data) {
       PAGE_BIT_WRITABLE | PAGE_BIT_PRESENT);
 
   // Unmap the bootloader image
-  map_pages2(&mm, data->bootloader_image_base, 0, data->bootloader_image_size, 0);
+  // map_pages2(&mm, data->bootloader_image_base, 0, data->bootloader_image_size, 0);
+
+  memset((void *)(data->pml4 + mm.virtual_offset), 0, 128 * 8);
 
   flush_page_table(&mm);
   log("Starting kernel");
@@ -105,20 +110,36 @@ void _start(BootData *data) {
   };
   enable_system_calls(&thread_context);
 
-  // vaddr_t user_entry;
-  // load_elf_file(&data->alloc, data->pml4, data->user_efi_file, &user_entry);
+  paddr_t user_pml4_addr = alloc_pages2(&page_alloc, 1);
 
-  // // TODO: Create a protection for the stack
-  // uint8_t *user_stack = alloc(&mm, PAGE_SIZE * 4);
-  // uint8_t *user_stack_top = user_stack + 4 * PAGE_SIZE - 1;
-  // flush_page_table(&mm);
+  PageTable *user_pml4 = (void *)(user_pml4_addr + mm.virtual_offset);
+  memcpy(user_pml4, (void *)(mm.pml4 + mm.virtual_offset), sizeof(*user_pml4));
 
-  // log("Running user program");
-  // console.fg = 0x00ff00ff;
-  // size_t status;
-  // status = run_user_program((void *)user_entry, (void *)user_stack_top);
-  // DEBUGD(status);
-  // console.fg = WHITE;
+  MemoryManager user_mm1 = {
+    .page_alloc = &page_alloc,
+    .start = PAGE_SIZE, // NOTE: Skip first null page
+    .end = HIGHER_HALF,
+    .first_object = 0,
+    .objects_count = 1, // NOTE: first object is null
+    .virtual_offset = HIGHER_HALF,
+    .pml4 = user_pml4_addr,
+  };
+
+  flush_page_table(&user_mm1);
+
+  vaddr_t user_entry;
+  load_elf_file2(&user_mm1, USER_FILE, &user_entry);
+
+  // TODO: Create a protection for the stack
+  uint8_t *user_stack = alloc(&user_mm1, 8 * PAGE_SIZE);
+  uint8_t *user_stack_top = user_stack + 8 * PAGE_SIZE - 1;
+
+  log("Running user program");
+  console.fg = 0x00ff00ff;
+  size_t status;
+  status = run_user_program((void *)user_entry, (void *)user_stack_top);
+  DEBUGD(status);
+  console.fg = WHITE;
 
   ASM("sti");
 
